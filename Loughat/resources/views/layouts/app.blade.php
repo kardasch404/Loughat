1fe2e4cbd7e8d2b4402b00707f730b808dadfd53
unknown <zz240614@gmail.com>
2025-03-31 12:47:15 +0000

    add layout && componant 4 the signup page => including header

---
+++ Loughat/resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('assets/dist/main.css') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('dist/images/favicon/favicon.png') }}" />
+</head>
+<body onload="loader()">
+    @include('component.header') 
+
+    <div >
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('assets/src/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/src/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/src/scss/vendors/plugin/js/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/src/scss/vendors/plugin/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/src/scss/vendors/plugin/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/src/scss/vendors/plugin/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/src/js/app.js') }}"></script>
+    <script src="{{ asset('assets/dist/main.js') }}"></script>
+</body>
+</html>
--- Loughat/routes/web.php
+++ Loughat/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+Route::post('/register', [JWTAuthController::class, 'register'])->name('jwt.register');
