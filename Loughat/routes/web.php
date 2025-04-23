92e5d11e737d57f02d000ad29f2261c5ea9b891a
unknown <zz240614@gmail.com>
2025-04-23 14:01:22 +0100

    implemnted pyemnt method scripe

--- Loughat/routes/web.php
+++ Loughat/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\CommandeController;
 use App\Http\Controllers\CoursController;
 use App\Http\Controllers\JWTAuthController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -165,3 +166,13 @@ Route::get('/cours/{coursId}', [CoursController::class, 'show'])->name('cours');
 // => -/Commande$
 Route::post('/commande', [CommandeController::class, 'store'])->name('commande');
 
+// => -/Payment
+Route::get('/payment', [PaymentController::class, 'showPaymentForm'])->name('payment.form');
+Route::post('/process-payment', [PaymentController::class, 'processPayment'])->name('process.payment');
+Route::get('/payment/success', function () {
+    return view('payment-success');
+})->name('payment.success');
+
+Route::get('/payment/failure', function () {
+    return view('payment-failure');
+})->name('payment.failure');
