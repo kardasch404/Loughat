ec5727e7c61c0a263c934642f2e1b766737594b9
unknown <zz240614@gmail.com>
2025-05-04 12:46:03 +0100

    add form scrollable in login and register form

--- Loughat/public/assets/user/src/js/app.js
+++ Loughat/public/assets/user/src/js/app.js
@@ -27,6 +27,28 @@ function openSearch() {
 function closeSearch() {
   document.getElementById('myOverlay').style.display = 'none';
 }
+// Function to show/hide password
+function showPassword(inputId, icon) {
+  const input = document.getElementById(inputId);
+  if (input.type === "password") {
+      input.type = "text";
+      icon.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off">
+      <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+      <line x1="1" y1="1" x2="23" y2="23"></line>
+  </svg>
+`;
+  } else {
+      input.type = "password";
+      icon.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+      <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+`;
+  }
+}
+
 
 
 // watch course 
@@ -112,6 +134,7 @@ document.getElementById('filter').addEventListener('click', function (e) {
   });
   mainForm.submit();
 });
+
 
 
 
